c29d8e1faf40483a942b79a833130299937b0704
swymmwys <>
2025-07-07 22:13:26 +0300

    update deps; fix config to handle "base" in dev mode

--- ui_web/vite.config.ts
+++ ui_web/vite.config.ts
@@ -10,7 +10,7 @@ import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: `/${process.env.gh_repo.split('/')[1]}/`,
+  base: process.env.NODE_ENV === 'production' ? `/${process.env.gh_repo.split('/')[1]}/` : '',
   plugins: [
     viteStaticCopy({
       targets: [
